5952d820cae9b40e8d44da81e207c63c30934117
Raphael <SundownDEV@users.noreply.github.com>
2018-01-17 02:32:00 +0100

    update dev

--- tests/routes.js
+++ tests/routes.js
@@ -2,12 +2,27 @@
  * Router calls
  */
 router.get('#/home', function(){
-  console.log('home!');
+    /* do something */
+    console.log('home!');
 });
 
-router.get('#/contact', function(){
-  /* do something */
-  console.log('contact!');
+router.get('#/abcd', function(){
+    /* do something */
+    console.log('abcd!');
 });
 
-router.run();
+router.get('#/hello', function(id){
+    console.log('test1!');
+});
+
+router.get('#/hello/world', function(){
+    console.log('test2!');
+});
+
+router.get('#/hello/world', function(id){
+    console.log('test2!');
+});
+
+router.run(function(){
+    /* Optional finish callback */
+});
