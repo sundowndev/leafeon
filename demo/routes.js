4dbda9bb6f019dde0d91a193e3053c34db318881
SundownDEV <anonhq789@protonmail.com>
2018-01-22 17:33:33 +0100

    parameter handling fix

--- demo/routes.js
+++ demo/routes.js
@@ -1,13 +1,18 @@
 /*
  * Router calls
  */
+router.add('', function(){
+    /* do something */
+    console.log('root');
+});
+
 router.add('#/home', function(){
     /* do something */
     console.log('home');
 });
 
 router.add('#/hello/world', function(){
-    console.log('hello world');
+    console.log('hello world :)');
 });
 
 router.add('#/hello/:name', function(name){
@@ -18,8 +23,10 @@ router.add('#/:name/is/here', function(name){
     console.log(name + ' is here!');
 });
 
+router.add('#/lol/:name/:token', function(name, token){
+    console.log(name + ' is here ' + token);
+});
+
 router.run(function(){
     /* Optional finish callback */
 });
-
-router.goto('#/lol/is/here');
