4ae9b0f009441c6d9a84ae7529d4664dfbe4879c
sundowndev <raphael@crvx.fr>
2018-12-05 18:00:23 +0100

    Unit tests

--- test/generator.spec.ts
+++
@@ -1,5 +0,0 @@
-const router = require('../src/leafeon');
-
-const leafeon = router.router();
-
-console.log(leafeon.routes);
---
+++ test/router.spec.ts
@@ -0,0 +1,38 @@
+const assert = require('assert');
+const sinon = require('sinon');
+
+const leafeon = require('../src/leafeon');
+
+const MockBrowser = require('mock-browser').mocks.MockBrowser;
+const mock = new MockBrowser();
+
+before(() => {});
+after(() => {});
+
+describe('Router.add', function() {
+  it('should return route name', function() {
+    const r = new leafeon.router();
+
+    r.add('test', '/test', () => {});
+
+    assert.equal(r.routes[0].name, 'test');
+  });
+
+  it('should return route path', function() {
+    const r = new leafeon.router();
+
+    r.add('test', '/test', () => {});
+
+    assert.equal(r.routes[0].path, '/test');
+  });
+});
+
+describe('Router.generateURL', function() {
+  it('should return route with params', function() {
+    const r = new leafeon.router();
+
+    const path = r.generateURL('/account/:id/settings/:section', {id: 1, section: 'password'});
+
+    assert.equal(path, '/account/1/settings/password');
+  });
+});
