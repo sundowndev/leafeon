73304981ff2a90cb525e2a401daa4c1508d5cab7
sundowndev <raphael@crvx.fr>
2018-12-06 14:05:45 +0100

    Unit tests

--- test/router.spec.ts
+++ test/router.spec.ts
@@ -6,33 +6,62 @@ const leafeon = require('../src/leafeon');
 //const MockBrowser = require('mock-browser').mocks.MockBrowser;
 //const mock = new MockBrowser();
 
-before(() => {});
-after(() => {});
+/*before(() => {});
+after(() => {});*/
 
-describe('Router.add', function() {
-  it('should return route name', function() {
-    const r = new leafeon.router();
+const r = new leafeon.Router();
 
-    r.add('test', '/test', () => {});
+describe('Router.add', () => {
+  it('should return route name', () => {
+    r.add('test', '/test', () => {
+      // code
+    });
 
     assert.equal(r.routes[0].name, 'test');
   });
 
-  it('should return route path', function() {
-    const r = new leafeon.router();
-
-    r.add('test', '/test', () => {});
+  it('should return route path', () => {
+    r.add('test', '/test', () => {
+      // code
+    });
 
     assert.equal(r.routes[0].path, '/test');
   });
 });
 
-describe('Router.generateURL', function() {
-  it('should return route with params', function() {
-    const r = new leafeon.router();
-
+describe('Router.generateURL', () => {
+  it('should return route with params', () => {
     const path = r.generateURL('/account/:id/settings/:section', {id: 1, section: 'password'});
 
     assert.equal(path, '/account/1/settings/password');
+  });
+});
+
+describe('Router.formatPath', () => {
+  it('should return formated path', () => {
+    const path = '/#/foo_bar';
+    const formated = r.formatPath(path, true);
+
+    assert.equal(formated, '/foo_bar');
+  });
+
+  it('should return path formated', () => {
+    const path = '#/docs/getting-started';
+    const formated = r.formatPath(path);
+
+    assert.equal(formated, '/docs/getting-started');
+  });
+
+  it('should return path formated', () => {
+    const path = '/hello/:name';
+    const formated = r.formatPath(path);
+
+    assert.equal(formated, '/hello/:name');
+  });
+
+  it('should throw exception', () => {
+    const path = '/foo*bar';
+
+    assert.throws(() => { r.formatPath(path); }, Error, 'Path is not formated correctly');
   });
 });
